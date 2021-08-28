a0994dc0a741eea6b8ff4bf3a2c32fd39bb4aacc
mahsak01 <mahsakarimi01@yahoo.com>
2021-08-28 16:07:22 +0430

    add home page to app

---
+++ quizgame_app/lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const SecondColor= Color(0xff45474b);
+const WhiteColor = Color(0xFFffffff);
+const GreenColor = Color(0xFF6AC259);
+const RedColor = Color(0xFFE92E30);
+const GrayColor = Color(0xFFBBBBBB);
+const BlackColor = Color(0xFF101010);
+const PrimaryGradient = LinearGradient(
+  colors: [Color(0xFF52ffd7), Color(0xFF10e3ca)],
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+);
+
+const double kDefaultPadding = 20.0;
---
+++ quizgame_app/lib/screens/home/home_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:quizgame_app/constants.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack( children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Image.asset(
+            "assets/background.png",
+            fit: BoxFit.fill,
+          ),
+        ),
+        SafeArea(
+            child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(flex: 2),
+                  Text(
+                    "Let's Play Quiz Game",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4!
+                        .copyWith(fontWeight: FontWeight.bold, color: WhiteColor),
+                  ),
+                  Text("Enter your name below"),
+                  Spacer(),
+                  TextField(
+
+                    decoration: InputDecoration(
+
+                        filled: true,
+                        fillColor: SecondColor,
+                        hintText: "Full Name",
+                        hintStyle: TextStyle(color: GrayColor,fontSize: 20),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            borderSide: BorderSide(color: GrayColor)),
+                      focusedBorder:  OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide: BorderSide(color: GrayColor))
+                    ),
+                  ),
+                  Spacer(),
+                  Center(
+                    child: GestureDetector(
+                      // on tap
+                      child: Container(
+                        height: 60,
+                        width: MediaQuery.of(context).size.width/2,
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.all(kDefaultPadding * 0.75),
+                        // 15
+                        decoration: BoxDecoration(
+                          gradient: PrimaryGradient,
+                          borderRadius: BorderRadius.all(Radius.circular(25)),
+                        ),
+                        child: Text(
+                          "Lets Start",
+                          style: Theme.of(context)
+                              .textTheme
+                              .button!
+                              .copyWith(color: Colors.black,fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Spacer(flex: 2), // it will take 2/6 spaces
+                ],
+              ),
+            ))
+      ]),
+    );
+  }
+}
